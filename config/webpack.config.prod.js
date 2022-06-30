5d506cce6658f69d384f78048262d8057e1d8b92
wangzhongyong <wzy@kgtdata.com>
2022-06-30 17:47:47 +0800

    publicPath

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -6,5 +6,18 @@ const common = require('./webpack.config.common');
 const { merge } = require('webpack-merge');
 module.exports = merge(common, {
     mode: 'production',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+
+        clean: true, //  new CleanWebpackPlugin()功能一样
+        // filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js', //hash 避免浏览器缓存
+        // filename: 'webpack-numbers.js',
+        publicPath: '/web',
+        // library: {
+        //     name: 'webpackNumbers',
+        //     type: 'umd',
+        // },
+    },
     devtool: 'eval-cheap-module-source-map', //inline-source-map source-map eval-cheap-module-source-map
 });
